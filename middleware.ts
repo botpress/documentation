671808e3b0282584824cff213c530e2012114a2a
lakshya <lakshyadahiya0246@gmail.com>
2023-06-29 19:10:56 -0400

    fix: routing to /api-documentation is broken

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-
-export function middleware(request: NextRequest) {
-  // Need to redirect because if we don't have /api-documentation/api-documentation.mdx it doesn't show up in the header
-  return NextResponse.redirect(new URL('/docs/api-documentation', request.url))
-}
-export const config = {
-  matcher: '/api-documentation/api-documentation',
-}
