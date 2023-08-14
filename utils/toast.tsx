14ca9d86d3dd97440f0b5657825ba0e05f2c7150
lakshya <lakshyadahiya0246@gmail.com>
2023-08-14 14:42:42 -0400

    chore: remove redundant type guard

--- utils/toast.tsx
+++ utils/toast.tsx
@@ -1,10 +1,11 @@
 import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline'
 import classNames from 'classnames'
 import hotToast from 'react-hot-toast'
-type ToastConfig = {
+export type ToastConfig = {
   message: string
 }
 type ToastVariant = 'success' | 'error'
+type ToastConfigOrMessage = ToastConfig | string
 
 const TOAST_VARIANT_CLASSNAMES: Record<ToastVariant, string> = {
   success: 'bg-green-300 text-green-800 dark:bg-green-900 dark:text-zinc-50',
@@ -14,14 +15,13 @@ const TOAST_VARIANT_ICONS: Record<ToastVariant, typeof ExclamationCircleIcon> = 
   success: CheckCircleIcon,
   error: ExclamationCircleIcon,
 }
-
-function getToastVariant(toastConfig: ToastConfig, variant: ToastVariant) {
+function getToastVariant(toastConfigOrMessage: ToastConfigOrMessage, variant: ToastVariant) {
   return hotToast.custom((_toast) => {
     const Icon = TOAST_VARIANT_ICONS[variant]
     return (
       <div className={classNames('flex rounded-md py-2 pr-4', TOAST_VARIANT_CLASSNAMES[variant])}>
         <Icon className="mx-2 h-6 w-6" />
-        {toastConfig.message}
+        {typeof toastConfigOrMessage === 'string' ? toastConfigOrMessage : toastConfigOrMessage.message}
       </div>
     )
   })
@@ -30,7 +30,7 @@ function getToastVariant(toastConfig: ToastConfig, variant: ToastVariant) {
 /**
  * To update default toast behaviour, go to `src/_app.mdx`
  */
-export const toast: Record<ToastVariant, (toastConfig: ToastConfig) => void> = {
+export const toast: Record<ToastVariant, (toastConfigOrMessage: ToastConfigOrMessage) => void> = {
   success: (toastConfig) => getToastVariant(toastConfig, 'success'),
   error: (toastConfig) => getToastVariant(toastConfig, 'error'),
 }
