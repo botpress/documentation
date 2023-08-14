a86476cef165171be981a9991699020217cc2115
lakshya <lakshyadahiya0246@gmail.com>
2023-08-14 12:50:19 -0400

    chore: add secondary button variant

--- components/SdkDiagram/SdkDiagram.tsx
+++ components/SdkDiagram/SdkDiagram.tsx
@@ -4,6 +4,7 @@ export function SdkDiagram() {
   return (
     <div>
       <button className="button primary">Documentation</button>
+      <button className="button secondary">Documentation</button>
     </div>
   )
 }
