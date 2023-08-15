c339e313a80842eeb4b40e0d29990626a0096351
lakshya <lakshyadahiya0246@gmail.com>
2023-08-15 17:24:51 -0400

    feat(diagram): add directional arrows

--- components/SdkDiagram/SdkDiagram.tsx
+++ components/SdkDiagram/SdkDiagram.tsx
@@ -49,7 +49,7 @@ const initialNodes: Node[] = [
   {
     id: '3',
     type: 'botpress',
-    position: { x: 550, y: 650 },
+    position: { x: 550, y: 700 },
     data: {
       label: 'Mail Shrimp',
       icon: BotIcon,
@@ -61,15 +61,41 @@ const initialNodes: Node[] = [
     } as BotpressNodeData,
   },
 ]
-const initialEdges: Edge[] = [
-  { id: 'e1-2', source: '1', target: '2', targetHandle: 'lt', type: 'smoothStepWithLabel', markerStart: 'external' },
-  { id: 'e2-1', source: '2', target: '1', type: 'smoothStepWithLabel', markerStart: INTEGRATION_SOURCE_MARKER_ID },
+type EdgeData = { color: string }
+const initialEdges: Edge<EdgeData>[] = [
+  {
+    id: 'e1-2',
+    source: '1',
+    target: '2',
+    targetHandle: 'lt',
+    type: 'smoothStepWithLabel',
+    markerStart: 'external',
+  },
+  {
+    id: 'e2-1',
+    source: '2',
+    target: '1',
+    type: 'smoothStepWithLabel',
+    markerStart: INTEGRATION_SOURCE_MARKER_ID,
+    data: { color: '#f0abfc' },
+  },
+  {
+    id: 'e2-3',
+    source: '2',
+    target: '3',
+    sourceHandle: 'rb',
+    type: 'smoothStepWithLabel',
+    markerStart: INTEGRATION_SOURCE_MARKER_ID,
+    data: { color: '#f0abfc' },
+  },
   {
     id: 'e3-2',
     source: '3',
     target: '2',
     type: 'smoothStepWithLabel',
     targetHandle: 'lb',
+    sourceHandle: 'rb',
+    data: { color: '#93c5fd' },
     markerStart: BOT_SOURCE_MARKER_ID,
   },
 ]
@@ -285,7 +311,8 @@ function SmoothStepWithLabelEdge({
   style = {},
   markerEnd,
   markerStart,
-}: EdgeProps) {
+  data,
+}: EdgeProps<EdgeData>) {
   const [edgePath, labelX, labelY] = getSmoothStepPath({
     sourceX,
     sourceY,
@@ -295,7 +322,6 @@ function SmoothStepWithLabelEdge({
     targetPosition,
   })
   const labelOrientation = useMemo(() => computeLabelOrientation(), [sourceX, sourceY, targetX, targetY])
-
   function computeLabelOrientation(): 'left' | 'right' | 'top' | 'bottom' {
     const horizontal = sourceX < targetX ? 'right' : 'left'
     const vertical = sourceY < targetY ? 'bottom' : 'top'
@@ -304,20 +330,43 @@ function SmoothStepWithLabelEdge({
     const horizontalMagnitude = Math.abs(sourceX - targetX)
     return verticalMagnitude > horizontalMagnitude ? vertical : horizontal
   }
-
+  const defaultColor = '#d4d4d8'
   return (
     <>
-      <BaseEdge path={edgePath} markerEnd={markerEnd} markerStart={markerStart} style={style} />
+      <BaseEdge path={edgePath} markerEnd={markerEnd} markerStart={markerStart} style={{ stroke: defaultColor }} />
       <EdgeLabelRenderer>
         <div
           style={{
             position: 'absolute',
             transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
             fontSize: 12,
+            color: data?.color ?? defaultColor,
           }}
           className="nodrag nopan"
         >
-          {labelOrientation}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="18"
+            viewBox="0 0 16 18"
+            fill="none"
+            className={classNames({
+              'rotate-180': labelOrientation === 'left',
+              '-rotate-90': labelOrientation === 'top',
+              'rotate-90': labelOrientation === 'bottom',
+            })}
+          >
+            <rect width="16" height="18" transform="matrix(1 0 0 -1 0 18)" fill="white" />
+            <path
+              opacity="0.5"
+              d="M0 2.50024L4.49997 8.5002L-5.24533e-07 14.5002L3.74997 14.5002L8.24994 8.5002L3.74997 2.50024L0 2.50024Z"
+              fill="currentColor"
+            />
+            <path
+              d="M5.25 2.50024L9.74997 8.5002L5.25 14.5002L8.99997 14.5002L13.4999 8.5002L8.99997 2.50024L5.25 2.50024Z"
+              fill="currentColor"
+            />
+          </svg>
         </div>
       </EdgeLabelRenderer>
     </>
