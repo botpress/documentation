3fbeeeca78961f99436fa66a1912d13cdcfeaad4
lakshya <lakshyadahiya0246@gmail.com>
2023-08-17 15:29:18 -0400

    fix(diagram): dark mode and layout

--- components/SdkDiagram/DocumentationCard.tsx
+++ components/SdkDiagram/DocumentationCard.tsx
@@ -8,12 +8,14 @@ export const DocumentationCard = forwardRef<HTMLDivElement, Props>((props, ref) 
       {...props}
       ref={ref}
       className={classNames(
-        'rounded-lg border border-zinc-200 bg-white dark:border-zinc-800 dark:bg-zinc-900',
+        'max-w-[310px] rounded-lg border border-zinc-200 bg-white dark:border-zinc-800 dark:bg-zinc-900',
         props.className
       )}
     >
-      <div className="border-b border-zinc-200 p-3 px-4 text-zinc-500 dark:border-zinc-800">Handler</div>
-      <div className="m-w-[280px] p-3 px-4 text-sm text-zinc-600">
+      <div className="border-b border-zinc-200 p-3 px-4 text-zinc-500 dark:border-zinc-800 dark:text-zinc-300">
+        Handler
+      </div>
+      <div className="p-3 px-4 text-sm text-zinc-600 dark:text-zinc-400">
         <p>
           The handler function is used to handle the incoming requests from the integration. Which means that this
           function will be called every time the telegram integration sends a request to the webhook url set in the
@@ -25,10 +27,10 @@ export const DocumentationCard = forwardRef<HTMLDivElement, Props>((props, ref) 
           implementation.
         </p>
         <div className="mt-4 flex gap-2">
-          <button className="button primary icon-trailing">
+          <button className="button primary icon-trailing w-full">
             Code <ArrowTopRightOnSquareIcon />
           </button>
-          <button className="button secondary icon-trailing">
+          <button className="button secondary icon-trailing w-full">
             Documentation <ArrowRightIcon />
           </button>
         </div>
